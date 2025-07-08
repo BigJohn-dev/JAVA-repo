6bdab40c2daf4be4c1da51faab08c2e7188d4a3e
John Imeobong <imeobongjohn38@gmail.com>
2025-07-08 21:11:19 +0100

    Add files via upload

--- Chapter 6/src/DietelChapter6/GreatestCommonDivisor.java
+++ Chapter 6/src/DietelChapter6/GreatestCommonDivisor.java
@@ -1,4 +1,20 @@
 package DietelChapter6;
 
+import java.util.Scanner;
+
 public class GreatestCommonDivisor {
+
+    public static int gdcIs(int number1, int number2) {
+        while (number2 != 0) {
+            int temp = number2;
+            number2 = number1 % number2;
+            number1 = temp;
+        }
+        return number1;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(gdcIs(56, 12));
+    }
 }
